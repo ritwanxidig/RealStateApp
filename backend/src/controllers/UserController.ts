0c02e65654d9329be4d4951f01fa051f66f02753
ritwanxidig <ritwanrashit@gmail.com>
2023-12-17 14:09:09 +0300

    added: getuserById functionality and its endpoint

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,15 +1,30 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
-import { getAll } from "../models/User";
+import { getAll, getById } from "../models/User";
 
 export const getAllUsers = async (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
   try {
     const users = await getAll();
     return res.status(200).json(users);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getUserById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const user = await getById(id);
+    const { name, username, email } = user;
+    return res.status(200).json({ name, username, email });
   } catch (error) {
     return next(error);
   }
--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,5 +1,5 @@
 import { IsAuthenticated, IsAuthorized } from "../middleware";
-import { getAllUsers } from "../controllers/UserController";
+import { getAllUsers, getUserById } from "../controllers/UserController";
 import express from "express";
 import { Roles } from "../constants";
 
@@ -9,5 +9,10 @@ export default (router: express.Router) => {
     IsAuthenticated,
     IsAuthorized([Roles.Admin, Roles.User]),
     getAllUsers
+  );
+  router.get(
+    "/users/:id",
+    IsAuthenticated,
+    getUserById
   );
 };
