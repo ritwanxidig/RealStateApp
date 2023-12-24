95c38db00025f86055ab376c18f7e26a78ff1a6f
ritwanxidig <ritwanrashit@gmail.com>
2023-12-24 09:36:29 +0300

    completed update property route endpoint

--- backend/src/routes/propertyRouter.ts
+++ backend/src/routes/propertyRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import {
-  createProperty,
+  addProperty,
+  editProperty,
   getAllProperties,
 } from "../controllers/PropertyController";
 import { IsAuthenticated } from "../middleware";
 
 export default (router: Router) => {
   router.get("/properties", getAllProperties);
-  router.post("/properties", IsAuthenticated, createProperty);
+  router.post("/properties", IsAuthenticated, addProperty);
+  router.put("/properties/:id", IsAuthenticated, editProperty);
 };
