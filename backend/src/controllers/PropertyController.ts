1a16498d996a0e0dcb005053f16ceada1fbaec87
ritwanxidig <ritwanrashit@gmail.com>
2023-12-24 08:48:11 +0300

    updated create and getAll properties functionalities

--- backend/src/controllers/PropertyController.ts
+++ backend/src/controllers/PropertyController.ts
@@ -2,6 +2,14 @@ import express, { Request, Response, NextFunction } from "express";
 import { PropertyModel, create, getAll } from "../models/Property";
 import { errorHandler } from "../utils";
 import { get, identity } from "lodash";
+import { IPropertyDDO } from "../interfaces/IPropertyDDO";
+import {
+  getCityById,
+  getCountryById,
+  getLocationById,
+} from "../models/Address";
+import { getById } from "../models/User";
+import { ObjectId } from "mongoose";
 
 export const getAllProperties = async (
   req: Request,
@@ -9,7 +17,52 @@ export const getAllProperties = async (
   next: NextFunction
 ) => {
   try {
-    const properties = await getAll();
+    const data = await getAll();
+
+    const properties = await Promise.all(
+      data.map(async (property) => {
+        const createdUser = await getById(property.userRef.toString());
+        const country = await getCountryById(property.address.country);
+        // city holds an object of one country with array of single city:
+        //  like {_id: "5f8f6e0e1b7f9c0f1b7f9c0f", name: "kathmandu", cities: [{ _id: "5f8f6e0e1b7f9c0f1b7f9c0f", name: "kathmandu", locations: [{ _id: "5f8f6e0e1b7f9c0f1b7f9c0f", name: "kathmandu" }] }]}
+        const city = await getCityById(property.address.city, country.name);
+
+        // location is same as city.locations[0]
+        const location = await getLocationById(
+          country.name,
+          city.name,
+          property.address.location
+        );
+        const propertyDDO: IPropertyDDO = {
+          name: property.name,
+          description: property.description,
+          price: property.price,
+          discount: property.discount,
+          imageUrls: property.imageUrls,
+          type: property.type,
+          beds: property.beds,
+          baths: property.baths,
+          furnished: property.furnished,
+          parking: property.parking,
+          offer: property.offer,
+          area: property.area,
+          address: {
+            country: country.name,
+            city: city.cities[0].name,
+            location: location.cities[0].locations[0].name,
+          },
+          user: {
+            name: createdUser.name,
+            username: createdUser.username,
+            email: createdUser.email,
+            roles: createdUser.roles,
+          },
+        };
+        return propertyDDO;
+      })
+    );
+    // const actualData = await properties;
+
     return res.status(200).json(properties);
   } catch (error) {
     next(error);
@@ -33,6 +86,44 @@ export const createProperty = async (
     const loggedInUser = get(req, "identity") as Record<string, any>;
     const userRef = loggedInUser._id;
     const property = await create({ userRef, ...req.body });
+
+    if (!property) {
+      return next(errorHandler(400, "Property not created"));
+    }
+
+    const country = await getCountryById(address.country);
+    const city = await getCityById(address.city, address.country);
+    const location = await getLocationById(
+      address.country,
+      address.city,
+      address.location
+    );
+    const propertyDDO: IPropertyDDO = {
+      name: property.name,
+      description: property.description,
+      price: property.price,
+      discount: property.discount,
+      imageUrls: property.imageUrls,
+      type: property.type,
+      beds: property.beds,
+      baths: property.baths,
+      furnished: property.furnished,
+      parking: property.parking,
+      offer: property.offer,
+      area: property.area,
+      address: {
+        country: country.name,
+        city: city.cities[0].name,
+        location: location.cities[0].locations[0].name,
+      },
+      user: {
+        name: loggedInUser.name,
+        username: loggedInUser.username,
+        email: loggedInUser.email,
+        roles: loggedInUser.roles,
+      },
+    };
+
     return res.status(200).json(property);
   } catch (error) {
     next(error);
