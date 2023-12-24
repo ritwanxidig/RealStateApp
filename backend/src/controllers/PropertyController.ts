530afbb548d8c6a068358224d0577f60fda7b081
ritwanxidig <ritwanrashit@gmail.com>
2023-12-24 07:26:02 +0300

    added: get all and create property functionality

--- backend/src/controllers/PropertyController.ts
+++ backend/src/controllers/PropertyController.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
-import { PropertyModel, getAll } from "../models/Property";
+import { PropertyModel, create, getAll } from "../models/Property";
 import { errorHandler } from "../utils";
+import { get, identity } from "lodash";
 
 export const getAllProperties = async (
   req: Request,
@@ -10,6 +11,29 @@ export const getAllProperties = async (
   try {
     const properties = await getAll();
     return res.status(200).json(properties);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createProperty = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name, price, imageUrls, type, beds, baths, address } = req.body;
+  if (!name || !price || !imageUrls || !type || !beds || !baths || !address)
+    return next(
+      errorHandler(
+        400,
+        "please provide all essentail fields: name, price, imageUrls, type, beds, baths, address "
+      )
+    );
+  try {
+    const loggedInUser = get(req, "identity") as Record<string, any>;
+    const userRef = loggedInUser._id;
+    const property = await create({ userRef, ...req.body });
+    return res.status(200).json(property);
   } catch (error) {
     next(error);
   }
