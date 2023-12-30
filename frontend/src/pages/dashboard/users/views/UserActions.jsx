c5618d07838fb1adcd93280e8b69c92f5ddfee32
ritwanxidig <ritwanrashit@gmail.com>
2023-12-30 21:19:29 +0300

    feat: Add UserActions component for updating and deleting users
    Introduce a new UserActions component to handle user update and deletion
    functionality. This component integrates formik for form management, yup
    for validation, and redux for dispatching alerts. The component enhances
    user experience by providing seamless update and deletion capabilities,
    improving the overall user management process.
    
    Issue: #123

---
+++ frontend/src/pages/dashboard/users/views/UserActions.jsx
@@ -0,0 +1,131 @@
+import React from 'react'
+import * as yup from 'yup'
+import { useFormik } from 'formik'
+import { Box, CircularProgress, Typography } from '@mui/material'
+
+
+
+
+// for project
+import StyledButton from '../../../../components/shared/StyledButton'
+import CustomField from '../../../../components/form/CustomField'
+import { useDeleteUserMutation, useUpdateUserMutation } from '../../../../app/services/api'
+import { alertActions } from '../../../../app/slices/alertSlice'
+import { useDispatch } from 'react-redux'
+
+
+
+const validationSchemas = yup.object().shape({
+    name: yup.string('Enter your name').required('Name is required'),
+    email: yup.string('Enter your email').email('Enter a valid email').required('Email is required'),
+    // small upper case letter, 1 number username
+    username: yup
+        .string('Enter your username')
+        .min(4, 'Username should be of minimum 4 characters length')
+        .max(15, 'Username should be of maximum 15 characters length')
+        .matches(/^[a-zA-Z0-9]+$/, 'Username should contain only letters and numbers')
+        .required('Username is required'),
+    role: yup.string('Enter your role').required('Role is required'),
+})
+
+const UserActions = ({ data, setOnOpen }) => {
+    const [updateUser, { isLoading }] = useUpdateUserMutation();
+    const [deleteUser, { isLoading: isDeleting }] = useDeleteUserMutation();
+    const dispatch = useDispatch();
+    const formik = useFormik({
+        initialValues: {
+            name: data?.name || '',
+            email: data?.email || '',
+            username: data?.username || '',
+            role: data?.roles[0] || '',
+        },
+        validationSchema: validationSchemas,
+        onSubmit: async (values) => {
+            const param = data?._id
+            try {
+                await updateUser({ param, ...values }).unwrap().then(() => {
+                    dispatch(alertActions.setAlert({ type: 'success', message: 'User updated successfully' }));
+                    setOnOpen(false);
+                }).catch((err) => {
+                    console.log(err);
+                    dispatch(alertActions.setAlert({ message: err?.data?.message, type: 'error' }));
+                })
+            } catch (err) {
+                console.log(err);
+                dispatch(alertActions.setAlert({ message: err?.data?.message, type: 'error' }));
+            }
+        }
+    });
+    const { values, touched, errors, handleSubmit, handleChange, handleBlur, } = formik;
+
+    const handleDelete = async () => {
+        const param = data?._id
+        try {
+            await deleteUser(param).unwrap().then(() => {
+                dispatch(alertActions.setAlert({ type: 'success', message: 'User deleted successfully' }));
+                setOnOpen(false);
+            }).catch((err) => {
+                console.log(err);
+                dispatch(alertActions.setAlert({ message: err?.data?.message, type: 'error' }));
+            })
+        } catch (err) {
+            console.log(err);
+            dispatch(alertActions.setAlert({ message: err?.data?.message, type: 'error' }));
+        }
+    }
+
+
+
+    return (
+        <Box sx={{
+            p: 0,
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
+            <form>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                    <CustomField
+                        name={"name"} value={values.name} onChange={handleChange} error={errors.name} touched={touched.name} label={"Name"} id="name" input type="text" required
+                        onBlur={handleBlur}
+                    />
+                    <CustomField
+                        name={"email"} value={values.email} onChange={handleChange} error={errors.email} touched={touched.email} label={"Email"} id="email" input type="email" required
+                        onBlur={handleBlur}
+
+                    />
+                    <Box sx={{
+                        display: 'flex',
+                        gap: 2,
+                        alignItems: 'center'
+                    }}>
+                        <CustomField
+                            name={"username"} value={values.username} onChange={handleChange} error={errors.username} touched={touched.username} label={"Username"} id="username" input type="text" required
+                            onBlur={handleBlur}
+                        />
+                        <CustomField
+                            name={"role"} value={values.role} onChange={handleChange} error={errors.role} touched={touched.role} label={"Role"} id="role" select type="text" required
+                            onBlur={handleBlur}
+                        >
+                            <option value="">Select Role</option>
+                            <option value="admin">Admin</option>
+                            <option value="user">User</option>
+                        </CustomField>
+                    </Box>
+                </Box>
+            </form>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    gap: 1,
+                    mt: 3
+                }}
+            >
+                <StyledButton variant="outlined" disabled={isLoading} onClick={handleSubmit} >  Update  {isLoading && <CircularProgress size={20} />}  </StyledButton>
+                <StyledButton variant="outlined" color="red" disabled={isDeleting} onClick={handleDelete} >  Delete  {isDeleting && <CircularProgress size={20} />}  </StyledButton>
+            </Box>
+        </Box>
+    )
+}
+
+export default UserActions
