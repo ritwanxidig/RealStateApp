b1888b1836100c6327de48f34d371d082d41d496
ritwanxidig <ritwanrashit@gmail.com>
2023-12-30 14:17:40 +0300

    Create firebase.js to initialize Firebase app
    Added a new file firebase.js to initialize the Firebase app with the
    provided configuration, enabling the frontend to interact with Firebase
    services. This sets the groundwork for integrating Firebase
    functionality into the web app. No issues fixed.

---
+++ frontend/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_4N9aOiqaaJzLdHtw_qinQfqRdW5Xo-c",
+  authDomain: "ritwan-real-estate-app.firebaseapp.com",
+  projectId: "ritwan-real-estate-app",
+  storageBucket: "ritwan-real-estate-app.appspot.com",
+  messagingSenderId: "702368861557",
+  appId: "1:702368861557:web:f750245cbc2e776344d73c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
