ffe1a628329f3fad7e5999d91027c8408d79ddb2
ritwanxidig <ritwanrashit@gmail.com>
2023-12-30 14:19:25 +0300

    Refactor GoogleOAuth component for Google sign-in functionality
    Refactored GoogleOAuth component to handle Google sign-in using Firebase
    authentication. Introduced a new asynchronous function to initiate
    Google OAuth sign-in and dispatch relevant actions upon successful
    sign-in. Introduced loading indication during the sign-in process. Catch
    and display error messages when sign-in fails.
    
    Resolves: #123

--- frontend/src/pages/authPages/components/GoogleOAuth.jsx
+++ frontend/src/pages/authPages/components/GoogleOAuth.jsx
@@ -1,9 +1,46 @@
-import { Box, Button } from '@mui/material'
+import { Box, Button, CircularProgress } from '@mui/material'
 import { IconBrandGoogle } from '@tabler/icons-react'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import GoogleLogo from '../../../assets/images/logos/GoogleLogo.jsx'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
+import app from '../../../firebase.js'
+import { useDispatch } from 'react-redux'
+
+// project
+import { useSignInWithGoogleMutation } from '../../../app/services/api'
+import { alertActions } from '../../../app/slices/alertSlice.js'
+import { authActions } from '../../../app/slices/authSlice.js'
 
 const GoogleOAuth = () => {
+    const [signInWithGoogle, { isLoading }] = useSignInWithGoogleMutation();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const handleSignInWithGoogle = async () => {
+        try {
+            const provider = new GoogleAuthProvider();
+            const auth = getAuth(app);
+
+            const result = await signInWithPopup(auth, provider);
+            await signInWithGoogle({
+                id: result.user.uid,
+                email: result.user.email,
+                name: result.user.displayName,
+                profilePic: result.user.photoURL
+            }).unwrap().then((res => {
+                dispatch(authActions.login(res));
+                navigate("/app");
+            })).catch((er => {
+                dispatch(alertActions.setAlert({
+                    type: "error",
+                    message: er?.data?.message || "Something went wrong"
+                }))
+            }));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <Box>
             <Button
@@ -23,8 +60,12 @@ const GoogleOAuth = () => {
                         borderColor: 'grey.600',
                         color: 'grey.600',
                     }
-                }} >
+                }}
+                onClick={handleSignInWithGoogle}
+                disabled={isLoading}
+            >
                 <GoogleLogo width="30px" height="30px" />  SignIn Google
+                {isLoading && <CircularProgress size={20} />}
             </Button>
         </Box>
     )
