e59084cc1fecf1d34752d2ac989cd7ed937c4649
ritwanxidig <ritwanrashit@gmail.com>
2023-12-30 21:17:51 +0300

    Refactor user detail handling in Users List page
    Introduce functionality to display user details upon action button
    click, enhancing user interaction and providing easier access to user
    information. This change improves user experience and facilitates
    efficient user management. Fixes #1234.

--- frontend/src/pages/dashboard/users/list.jsx
+++ frontend/src/pages/dashboard/users/list.jsx
@@ -6,10 +6,19 @@ import { IconButton, Tooltip } from '@mui/material'
 import { IconDots, IconPlus } from '@tabler/icons-react'
 import AddUser from './views/AddUser'
 import { useGetUsersQuery } from '../../../app/services/api'
+import UserDetail from './views/UserDetail'
 
 const Users_List = () => {
   const [addOpen, setAddOpen] = React.useState(false);
+  const [detailOpen, setDetailOpen] = React.useState(false);
+  const [selectedUser, setSelectedUser] = React.useState(null);
   const { data: users, isFetching } = useGetUsersQuery();
+
+  const handleDetailOpen = (params) => {
+    setDetailOpen(true);
+    setSelectedUser(params?.row);
+  }
+
   const Actions = () => (
     <div className="flex gap-2">
       <Tooltip title="New User">
@@ -23,13 +32,22 @@ const Users_List = () => {
     { field: 'email', headerName: 'Email', width: 200 },
     { field: 'username', headerName: 'Username', width: 200 },
     { field: 'roles', headerName: 'Role', width: 200 },
-    { field: 'actions', headerName: 'Actions', width: 100, renderCell: () => <IconButton sx={{ backgroundColor: 'primary.main', color: 'white', ":hover": { backgroundColor: 'primary.dark' } }} > <IconDots /> </IconButton> },
+    {
+      field: 'actions', headerName: 'Actions', width: 100,
+      renderCell: (params) =>
+        <IconButton
+          onClick={() => handleDetailOpen(params)}
+          sx={{ backgroundColor: 'primary.main', color: 'white', ":hover": { backgroundColor: 'primary.dark' } }} >
+          <IconDots />
+        </IconButton>
+    },
   ];
   const rows = users?.map((user) => ({ ...user, id: user._id })) || [];
 
   return (
     <PageContainer title="Users Page" description='Users List Page'>
       {addOpen && <AddUser onOpen={addOpen} setOnOpen={setAddOpen} />}
+      {detailOpen && <UserDetail onOpen={detailOpen} data={selectedUser} setOnOpen={setDetailOpen} />}
       <PageCard headtitle='Users Management' title="Users List" headsubtitle="Page" action={<Actions />}>
         <StyledDataGrid columns={columns} data={rows} />
       </PageCard>
