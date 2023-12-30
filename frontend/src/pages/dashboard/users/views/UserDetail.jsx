9531223d372c3740d06027015e9fc926c770ef66
ritwanxidig <ritwanrashit@gmail.com>
2023-12-30 21:18:28 +0300

    Create UserDetail component for displaying user details in a modal This commit introduces the UserDetail component, which renders a modal containing tabs for displaying user information and actions. This component provides a structured and user-friendly interface for interacting with user details. It enhances the user experience when managing user data. No associated issues.

---
+++ frontend/src/pages/dashboard/users/views/UserDetail.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import CustomModal from '../../../../views/utilities/CustomModal'
+import { Box, Tab, Typography } from '@mui/material'
+import { TabPanel, TabList, TabContext } from "@mui/lab";
+import styled from '@emotion/styled';
+import UserInfo from './UserInfo';
+import UserActions from './UserActions';
+
+const UserDetail = ({ onOpen, setOnOpen, data }) => {
+  const [tabValue, setTabValue] = React.useState("1")
+
+  const Actions = () => {
+    <></>
+  }
+
+
+  const StyledTab = styled(Tab)(({ theme }) => (
+    {
+      textTransform: 'none',
+      borderRadius: '50px'
+    }
+  ))
+
+  return (
+    <CustomModal
+      title="User Detail"
+      onOpen={onOpen}
+      setOnOpen={setOnOpen}
+      actions={<Actions />}
+    >
+      <TabContext value={tabValue} >
+        <TabList
+          sx={{
+            mt: 1,
+            "& .MuiTabs-indicator": {
+              display: "none"
+            }
+          }}
+          variant="scrollable"
+          scrollButtons
+          allowScrollButtonsMobile
+          onChange={(event, newValue) => setTabValue(newValue)}
+        >
+          <StyledTab label="Basic Info" value="1" />
+          <StyledTab label="Actions" value="2" />
+        </TabList>
+        <TabPanel value="1">
+          <UserInfo data={data} />
+        </TabPanel>
+        <TabPanel value="2">
+          <UserActions data={data} setOnOpen={setOnOpen} />
+        </TabPanel>
+
+      </TabContext>
+
+
+
+    </CustomModal>
+  )
+}
+
+export default UserDetail
