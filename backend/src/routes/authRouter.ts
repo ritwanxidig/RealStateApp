c5d9bc9a719b127880524c6eb1946f72e9aa40ca
ritwanxidig <ritwanrashit@gmail.com>
2023-12-30 14:17:11 +0300

    Add signInWithGoogle endpoint to authRouter
    Expanded authRouter to include a new signInWithGoogle endpoint,
    enhancing user authentication options and enabling Google sign-in
    functionality. This change broadens the range of authentication methods
    available. #12

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,8 +1,14 @@
-import { logOut, login, registration } from "../controllers/AuthController";
+import {
+  logOut,
+  login,
+  registration,
+  signInWithGoogle,
+} from "../controllers/AuthController";
 import express from "express";
 
 export default (router: express.Router) => {
   router.post("/auth/register", registration);
   router.post("/auth/login", login);
+  router.post("/auth/google", signInWithGoogle);
   router.post("/auth/logout", logOut);
 };
