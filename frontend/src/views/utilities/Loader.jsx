1e48c79a325bf8588d828bc01722e6682df8a4c5
ritwanxidig <ritwanrashit@gmail.com>
2024-01-02 11:58:43 +0300

    Create Loader component for displaying a themed loading spinner
    A new Loader component is added to display a themed loading spinner
    using react-spinners. This component will be used to indicate loading
    states in the frontend views.
    
    #<ISSUE-REFERENCE>

---
+++ frontend/src/views/utilities/Loader.jsx
@@ -0,0 +1,15 @@
+import { useTheme } from '@emotion/react';
+import { Box } from '@mui/material';
+import React from 'react'
+import { CircleLoader, HashLoader } from "react-spinners";
+
+const Loader = () => {
+    const theme = useTheme();
+    return (
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', color: 'primary.main' }}>
+            <HashLoader color={theme.palette.primary.main} />
+        </Box>
+    )
+}
+
+export default Loader
