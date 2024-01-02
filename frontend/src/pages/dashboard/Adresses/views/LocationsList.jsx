b21df2e5c7510672440b4c31df127ab2558460e3
ritwanxidig <ritwanrashit@gmail.com>
2024-01-02 20:12:22 +0300

    Create LocationsList component for displaying and managing locations, with options to add new locations and edit existing ones. The component integrates with API data based on the selected country and city, rendering a list of locations with associated actions. This addition enhances the user interface and increases functionality for managing location data. No issue references.

---
+++ frontend/src/pages/dashboard/Adresses/views/LocationsList.jsx
@@ -0,0 +1,61 @@
+import { Box, IconButton, Tooltip, Typography } from '@mui/material'
+import React from 'react'
+
+// for project
+import { useGetLocationsQuery } from '../../../../app/services/api';
+import StyledDataGrid from '../../../../components/StyledDataGrid'
+import { IconDots, IconPlus } from '@tabler/icons-react';
+import Loader from '../../../../views/utilities/Loader';
+import AddCity from './AddCity';
+import EditLocation from './EditLocation';
+import AddLocation from './AddLocation';
+
+const LocationsList = ({ country, loading, city }) => {
+  const [addOpen, setAddOpen] = React.useState(false);
+  const [editOpen, setEditOpen] = React.useState(false);
+  const [selectedLocation, setSelectedLocation] = React.useState(null);
+
+  const { data, isFetching } = useGetLocationsQuery(`${country?.name}/${city?.name}`);
+  console.log(data);
+
+  const rows = data?.cities[0]?.locations?.map((item, i) => ({ ...item, index: i + 1, id: item._id }));
+
+  const handleEditOpen = (params) => {
+    setEditOpen(true);
+    setSelectedLocation(params);
+  }
+
+
+  if (loading) return <Loader />
+
+  return (
+    <>
+      {addOpen && <AddLocation onOpen={addOpen} setOnOpen={setAddOpen} country={country} city={city} />}
+      {editOpen && <EditLocation onOpen={editOpen} setOnOpen={setEditOpen} city={city} data={selectedLocation} country={country} />}
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Typography variant="body1" fontWeight="bold" fontFamily="Plus Jakarta Sans">Locations</Typography>
+          <Tooltip title="New City">
+            <IconButton onClick={() => setAddOpen(true)} sx={{ backgroundColor: 'primary.main', color: 'white', ":hover": { backgroundColor: 'primary.dark' } }} > <IconPlus size={16} /> </IconButton>
+          </Tooltip>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: .8, py: 3, width: '100%', height: '200px', mt: 1.5, overflow: 'auto' }}>
+          {isFetching ? <Loader /> :
+            rows && rows.length > 0 ?
+              rows?.map((location) => (
+                <Box key={location._id} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: 'primary.800', p: 1, borderRadius: '10px', ":hover": { backgroundColor: 'primary.700' }, transition: 'all 0.3s ease-in-out' }}>
+                  <Typography>{location.name}</Typography>
+                  <IconButton
+                    onClick={() => handleEditOpen(location)}
+                    sx={{ backgroundColor: 'primary.main', color: 'white', ":hover": { backgroundColor: 'primary.dark' } }} > <IconDots size={16} /> </IconButton>
+                </Box>
+              )) :
+              <Typography>No Data</Typography>
+          }
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+export default LocationsList
