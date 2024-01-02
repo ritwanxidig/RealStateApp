36e10fabfe01c51c662e74ffcb588390474a0dcc
ritwanxidig <ritwanrashit@gmail.com>
2024-01-02 20:07:09 +0300

    feat: Add CitiesList component for displaying and managing cities
    This commit introduces a new CitiesList component for displaying a list
    of cities, allowing users to add new cities and view city details. The
    component leverages data fetched from the API and provides interactive
    functionality for managing city data within the application.
    
    No related issues.

---
+++ frontend/src/pages/dashboard/Adresses/views/CitiesList.jsx
@@ -0,0 +1,57 @@
+import { Box, IconButton, Tooltip, Typography } from '@mui/material'
+import React from 'react'
+
+// for project
+import { useGetCitiesQuery } from '../../../../app/services/api';
+import StyledDataGrid from '../../../../components/StyledDataGrid'
+import { IconDots, IconPlus } from '@tabler/icons-react';
+import Loader from '../../../../views/utilities/Loader';
+import AddCity from './AddCity';
+import CityDetail from './CityDetail';
+
+const CitiesList = ({ country, loading }) => {
+    const [addOpen, setAddOpen] = React.useState(false);
+    const [detailOpen, setDetailOpen] = React.useState(false);
+    const [selectedCity, setSelectedCity] = React.useState(null);
+
+    const { data, isFetching } = useGetCitiesQuery(country?.name);
+
+    const rows = data?.cities?.map((item, i) => ({ ...item, index: i + 1, id: item._id }));
+
+    const handleDetailOpen = (params) => {
+        setDetailOpen(true);
+        setSelectedCity(params);
+    }
+
+
+    if (loading) return <Loader />
+
+    return (
+        <>
+            {addOpen && <AddCity onOpen={addOpen} setOnOpen={setAddOpen} country={country} />}
+            {detailOpen && <CityDetail onOpen={detailOpen} setOnOpen={setDetailOpen} data={selectedCity} country={country} />}
+            <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <Typography variant="body1" fontWeight="bold" fontFamily="Plus Jakarta Sans">Cities</Typography>
+                    <Tooltip title="New City">
+                        <IconButton onClick={() => setAddOpen(true)} sx={{ backgroundColor: 'primary.main', color: 'white', ":hover": { backgroundColor: 'primary.dark' } }} > <IconPlus size={16} /> </IconButton>
+                    </Tooltip>
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: .8, py: 3, width: '100%', height: '200px', mt: 1.5, overflow: 'auto' }}>
+                    {isFetching ? <Loader /> :
+                        rows && rows.length > 0 ?
+                            rows?.map((city) => (
+                                <Box key={city._id} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: 'primary.800', p: 1, borderRadius: '10px', ":hover": { backgroundColor: 'primary.700' }, transition: 'all 0.3s ease-in-out' }}>
+                                    <Typography>{city.name}</Typography>
+                                    <IconButton onClick={() => handleDetailOpen(city)} sx={{ backgroundColor: 'primary.main', color: 'white', ":hover": { backgroundColor: 'primary.dark' } }} > <IconDots size={16} /> </IconButton>
+                                </Box>
+                            )) :
+                            <Typography>No Data</Typography>
+                    }
+                </Box>
+            </Box>
+        </>
+    )
+}
+
+export default CitiesList
