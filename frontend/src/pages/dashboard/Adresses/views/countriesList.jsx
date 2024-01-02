9415199ad2ef166565f62b7a6fcd7321d316ef49
ritwanxidig <ritwanrashit@gmail.com>
2024-01-02 20:05:59 +0300

    feat: Add CountriesList component to display a list of countries with actions
    The new CountriesList component renders a list of countries with the
    ability to view details and add new countries. It utilizes data from the
    API and presents it in a structured and interactive manner, enhancing
    the user experience. This addition addresses the need for a dedicated
    view to manage countries effectively.
    
    Resolves: #123

---
+++ frontend/src/pages/dashboard/Adresses/views/countriesList.jsx
@@ -0,0 +1,73 @@
+import { Box, IconButton, Tooltip, Typography } from '@mui/material'
+import React from 'react'
+import { IconDots, IconPlus } from "@tabler/icons-react";
+
+// for project
+import { useGetCountriesQuery } from '../../../../app/services/api';
+import StyledDataGrid from '../../../../components/StyledDataGrid'
+import CountryDetails from './CountryDetails';
+import AddCountry from './AddCountry';
+
+const CountriesList = () => {
+    const { data, isFetching } = useGetCountriesQuery();
+    const [addOpen, setAddOpen] = React.useState(false);
+    const [detailOpen, setDetailOpen] = React.useState(false);
+    const [selectedCountry, setSelectedCountry] = React.useState(null);
+
+    const handleDetailOpen = (params) => {
+        setDetailOpen(true);
+        setSelectedCountry(params?.row);
+    }
+
+    const columns = [
+        {
+            field: 'index',
+            headerName: '#',
+            width: 60,
+        },
+        {
+            field: 'name',
+            headerName: 'Name',
+            width: 800,
+        },
+        {
+            field: 'actions',
+            headerName: 'Actions',
+            width: 90,
+            renderCell: (params) => (
+                <IconButton
+                    // onClick={() => handleDetailOpen(params)}
+                    sx={{ backgroundColor: 'primary.main', color: 'white', ":hover": { backgroundColor: 'primary.dark' } }}
+                    onClick={() => handleDetailOpen(params)}
+                >
+                    <IconDots />
+                </IconButton>
+            ),
+        }
+    ];
+
+    const rows = data?.countries?.map((item, i) => ({ ...item, index: i + 1, id: item._id }));
+
+    return (
+        <>
+            {addOpen && <AddCountry onOpen={addOpen} setOnOpen={setAddOpen} />}
+            {detailOpen && <CountryDetails onOpen={detailOpen} setOnOpen={setDetailOpen} data={selectedCountry} />}
+            <Box sx={{
+                display: 'flex', flexDirection: 'column', gap: 4, p: 3, width: '100%',
+                borderRadius: '20px',
+                boxShadow: 'rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+
+            }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <Typography variant="h6" fontFamily="Plus Jakarta Sans">Countries</Typography>
+                    <Tooltip title="New Country">
+                        <IconButton onClick={() => setAddOpen(true)} sx={{ backgroundColor: 'primary.main', color: 'white', ":hover": { backgroundColor: 'primary.dark' } }} > <IconPlus /> </IconButton>
+                    </Tooltip>
+                </Box>
+                <StyledDataGrid data={rows} columns={columns} loading={isFetching} />
+            </Box>
+        </>
+    )
+}
+
+export default CountriesList
