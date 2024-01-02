3645e69d81c95c35bf90590da18c11aefa7a522e
ritwanxidig <ritwanrashit@gmail.com>
2024-01-02 20:13:35 +0300

    Refactor addresses list layout to use MUI Box component
    Restructure addresses list layout to utilize MUI Box for improved
    organization and spacing. This change sets the foundation for future
    layout enhancements.

--- frontend/src/pages/dashboard/Adresses/list.jsx
+++ frontend/src/pages/dashboard/Adresses/list.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import PageContainer from '../../../Layout/Main/Containers/PageContainer'
 import PageCard from '../../../Layout/Main/Containers/PageCard'
+import { Box } from '@mui/material'
+import CountriesList from './views/countriesList'
+import CitiesList from './views/CitiesList'
+import LocationsList from './views/LocationsList'
 
 const Adresses_List = () => {
     return (
         <PageContainer title='Dashboard' description=''>
             <PageCard title='Adresses' headtitle="Adresses Management" headsubtitle="Page">
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4, p: 3, width: '100%' }}>
+                    <CountriesList />
+                </Box>
 
             </PageCard>
         </PageContainer>
