525257a45a09608cc2323ca1a80b438a5c1f4588
ritwanxidig <ritwanrashit@gmail.com>
2024-01-02 20:07:43 +0300

    Create CityDetail component and integrate city details and editing functionality
    Introduce a new CityDetail component to display city details and
    incorporate editing capabilities for city names. This allows users to
    view city information and modify city names as needed. Additionally, the
    component facilitates seamless navigation between city locations and
    various actions. The creation of this component enhances the user
    experience by providing a clear, organized interface for managing city
    details.
    
    #ISSUE-123

---
+++ frontend/src/pages/dashboard/Adresses/views/CityDetail.jsx
@@ -0,0 +1,83 @@
+import { Box, IconButton, Tab, Tooltip, Typography } from '@mui/material'
+import React from 'react'
+import { useGetCityQuery } from '../../../../app/services/api'
+import EditCity from './EditCity'
+import { TabContext, TabList, TabPanel } from '@mui/lab'
+import styled from '@emotion/styled'
+import { IconEdit } from '@tabler/icons-react'
+import Loader from '../../../../views/utilities/Loader'
+import LocationsList from './LocationsList'
+import CustomModal from '../../../../views/utilities/CustomModal'
+
+
+
+
+const StyledTab = styled(Tab)(({ theme }) => (
+    {
+        textTransform: 'none',
+        borderRadius: '50px'
+    }
+))
+const CityDetail = ({ onOpen, setOnOpen, data, country }) => {
+    const [tabValue, setTabValue] = React.useState("1")
+    const [editOpen, setEditOpen] = React.useState(false);
+    const { data: currentCity, isFetching: isFetchingCity } = useGetCityQuery(`${country?.name}/${data?._id}`);
+    return (
+        <>
+            {editOpen && <EditCity onOpen={editOpen} setOnOpen={setEditOpen} data={currentCity ? currentCity : data} country={country} />}
+
+            <CustomModal
+                title="City Detail"
+                onOpen={onOpen}
+                setOnOpen={setOnOpen}
+            >
+                <TabContext value={tabValue} >
+                    <TabList
+                        sx={{
+                            mt: 1,
+                            "& .MuiTabs-indicator": {
+                                display: "none"
+                            }
+                        }}
+                        variant="scrollable"
+                        scrollButtons
+                        allowScrollButtonsMobile
+                        onChange={(event, newValue) => setTabValue(newValue)}
+                    >
+                        <StyledTab label="Locations" value="1" />
+                        <StyledTab label="Actions" value="2" />
+                    </TabList>
+                    <TabPanel value="1">
+                        <Box>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: '',
+                                    gap: 2,
+                                    alignItems: 'center',
+                                    mb: 1
+                                }}>
+                                <Typography variant="body1" fontWeight="bold" fontFamily="Plus Jakarta Sans">{currentCity ? currentCity?.name : data?.name}</Typography>
+                                <Tooltip title="Edit Name">
+                                    <IconButton
+                                        onClick={() => setEditOpen(true)}
+                                        sx={{ backgroundColor: 'primary.main', color: 'white', ":hover": { backgroundColor: 'primary.dark' } }}
+                                    >
+                                        <IconEdit size={12} />
+                                    </IconButton>
+                                </Tooltip>
+                            </Box>
+                            {isFetchingCity ? <Loader /> : <LocationsList city={currentCity ? currentCity : data} country={country} loading={isFetchingCity} />}
+                        </Box>
+                    </TabPanel>
+                    <TabPanel value="2">
+                        <Box>Tab2</Box>
+                    </TabPanel>
+
+                </TabContext>
+            </CustomModal>
+
+        </>)
+}
+
+export default CityDetail
