0cfa2ded185004b838577bab13dc382165c26cff
ritwanxidig <ritwanrashit@gmail.com>
2024-01-04 19:56:48 +0300

    updated property router

--- backend/src/routes/propertyRouter.ts
+++ backend/src/routes/propertyRouter.ts
@@ -4,11 +4,15 @@ import {
   deleteProperty,
   editProperty,
   getAllProperties,
+  getMyProperty,
+  getProperty,
 } from "../controllers/PropertyController";
 import { IsAuthenticated } from "../middleware";
 
 export default (router: Router) => {
   router.get("/properties", IsAuthenticated, getAllProperties);
+  router.get("/properties/:id", IsAuthenticated, getProperty);
+  router.get("/my-properties", IsAuthenticated, getMyProperty);
   router.post("/properties", IsAuthenticated, addProperty);
   router.put("/properties/:id", IsAuthenticated, editProperty);
   router.delete("/properties/:id", IsAuthenticated, deleteProperty);
