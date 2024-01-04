93eb1936d3b745aa7502cbe972281ddfb4620f2f
ritwanxidig <ritwanrashit@gmail.com>
2024-01-04 19:53:12 +0300

    Refactor property controller to add new endpoints for retrieving user properties and individual property.
    - Add new endpoints for retrieving user properties and individual
    property details
    - Provide more comprehensive property details including user info,
    country, city, and location
    - Improve the overall functionality and flexibility of the property
    controller
    - Enhance the user experience by offering more detailed property
    information
    
    #<ISSUE-REF>

--- backend/src/controllers/PropertyController.ts
+++ backend/src/controllers/PropertyController.ts
@@ -3,6 +3,7 @@ import {
   createProperty,
   getAll,
   getByPropertyId,
+  getUserProperties,
   removePropertyById,
   updatePropertyById,
 } from "../models/Property";
@@ -73,6 +74,99 @@ export const getAllProperties = async (
       })
     );
     // const actualData = await properties;
+
+    return res.status(200).json(properties);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getProperty = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  try {
+    if (!isValidObjectId(id))
+      return next(errorHandler(400, "please insert valid objectId"));
+    const property = await getByPropertyId(id);
+    if (!property) return next(errorHandler(400, "property not found"));
+    const owner = await getById(property.userRef.toString());
+    const country = await getCountryById(property.address.country);
+    const city = await getCityById(
+      property.address.city,
+      property.address.country
+    );
+    const location = await getLocationById(
+      property.address.country,
+      property.address.city,
+      property.address.location
+    );
+
+    return res.status(200).json(property);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMyProperty = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const requestedUser = get(req, "identity") as Record<string, any>;
+    console.log(requestedUser);
+    const data = await getUserProperties(requestedUser._id);
+
+    const properties = await Promise.all(
+      data.map(async (property) => {
+        const createdUser = await getById(property.userRef.toString());
+        const country = await getCountryById(property.address.country);
+        // city holds an object of one country with array of single city:
+        //  like {_id: "5f8f6e0e1b7f9c0f1b7f9c0f", name: "kathmandu", cities: [{ _id: "5f8f6e0e1b7f9c0f1b7f9c0f", name: "kathmandu", locations: [{ _id: "5f8f6e0e1b7f9c0f1b7f9c0f", name: "kathmandu" }] }]}
+        const city = await getCityById(
+          property.address.city,
+          property.address.country
+        );
+
+        // location is same as city.locations[0]
+        const location = await getLocationById(
+          property.address.country,
+          property.address.city,
+          property.address.location
+        );
+        const propertyDDO: IPropertyDDO = {
+          _id: property._id.toString(),
+          _createdAt: property.createdAt,
+          _updatedAt: property.updatedAt,
+          name: property?.name,
+          description: property.description,
+          price: property.price,
+          discount: property.discount,
+          imageUrls: property.imageUrls,
+          type: property.type,
+          beds: property.beds,
+          baths: property.baths,
+          furnished: property.furnished,
+          parking: property.parking,
+          area: property.area,
+          address: {
+            country: country.name,
+            city: city.cities[0].name,
+            location: location?.cities[0]?.locations[0]?.name,
+          },
+          user: {
+            name: createdUser?.name,
+            username: createdUser?.username,
+            email: createdUser?.email,
+            roles: createdUser?.roles,
+          },
+        };
+        return propertyDDO;
+      })
+    );
 
     return res.status(200).json(properties);
   } catch (error) {
