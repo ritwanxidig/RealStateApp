760091e0415b079fca9bdc588b478e720d00825d
ritwanxidig <ritwanrashit@gmail.com>
2024-01-06 09:34:56 +0300

    Refactor CustomField component props handling
    Refactor CustomField component to receive input props separately for
    better customization and reusability. This change enables more
    flexibility in managing input props, leading to improved component
    extensibility and maintainability.
    
    Issue #123

--- frontend/src/components/form/CustomField.jsx
+++ frontend/src/components/form/CustomField.jsx
@@ -5,14 +5,14 @@ import CustomTextArea from './CustomTextArea'
 import CustomSelect from './CustomSelectField'
 import CustomCheckbox from './CustomCheckbox'
 
-const CustomField = ({ input, textarea, touched, select, checkbox, error, label, id, name, type, placeholder, value, onChange, onBlur, optional, ...props }) => {
+const CustomField = ({ input, inputProps, textarea, touched, select, checkbox, error, label, id, name, type, placeholder, value, onChange, onBlur, optional, ...props }) => {
     return (
         <Box>
             <label htmlFor={id} className='text-sm font-semibold' {...props}>{label}{optional ? <span className="text-gray-400">(optional)</span> : <span className='text-red-500'>*</span>}</label>
-            {input && (<CustomInput error={error} id={id} name={name} type={type} placeholder={placeholder} value={value} onChange={onChange} onBlur={onBlur} {...props} />)}
-            {textarea && (<CustomTextArea error={error} id={id} name={name} type={type} placeholder={placeholder} value={value} onChange={onChange} onBlur={onBlur} {...props} />)}
-            {select && (<CustomSelect error={error} id={id} name={name} type={type} placeholder={placeholder} value={value} onChange={onChange} onBlur={onBlur} {...props}> {props.children} </CustomSelect>)}
-            {checkbox && (<CustomCheckbox error={error} id={id} name={name} type={type} placeholder={placeholder} value={value} onChange={onChange} onBlur={onBlur} {...props} />)}
+            {input && (<CustomInput error={error} id={id} name={name} type={type} placeholder={placeholder} value={value} onChange={onChange} onBlur={onBlur} {...inputProps} {...props} />)}
+            {textarea && (<CustomTextArea error={error} id={id} name={name} type={type} placeholder={placeholder} value={value} onChange={onChange} onBlur={onBlur} {...inputProps} {...props} />)}
+            {select && (<CustomSelect error={error} id={id} name={name} type={type} placeholder={placeholder} value={value} onChange={onChange} onBlur={onBlur} {...inputProps} {...props}> {props.children} </CustomSelect>)}
+            {checkbox && (<CustomCheckbox error={error} id={id} name={name} type={type} placeholder={placeholder} value={value} onChange={onChange} onBlur={onBlur} {...inputProps} {...props} />)}
             {error && touched && <p className='text-sm text-red-500'>{error}</p>}
         </Box>
     )
