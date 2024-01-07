e71c0d36e5f8cc369865a87042ee8a5c61a5234f
ritwanxidig <ritwanrashit@gmail.com>
2024-01-07 08:01:59 +0300

    Refactor propertyRouter to separate public and private access
    This refactor separates public and private access endpoints to improve
    code organization and clarity, allowing for easier maintenance and
    future expansion. It also enhances security by clearly defining access
    levels for each endpoint.
    
    No associated issues.

--- backend/src/routes/propertyRouter.ts
+++ backend/src/routes/propertyRouter.ts
@@ -11,8 +11,11 @@ import {
 import { IsAuthenticated } from "../middleware";
 
 export default (router: Router) => {
+  // for public access
   router.get("/properties", getAllProperties);
-  router.get("/properties/search", IsAuthenticated, searchProperty);
+  router.get("/properties/search", searchProperty);
+
+  // for private access
   router.get("/properties/:id", IsAuthenticated, getProperty);
   router.get("/my-properties", IsAuthenticated, getMyProperty);
   router.post("/properties", IsAuthenticated, addProperty);
