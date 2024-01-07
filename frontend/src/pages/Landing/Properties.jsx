2f1ec7956ddd0402e3bf448f0bda76baca6847d3
ritwanxidig <ritwanrashit@gmail.com>
2024-01-07 11:22:00 +0300

    Refactor Properties component to add filter link
    Added a filter link to the Properties component to enable users to
    access all available filters easily, enhancing user experience and
    search functionality. This change improves navigation within the
    application. No related issues.

--- frontend/src/pages/Landing/Properties.jsx
+++ frontend/src/pages/Landing/Properties.jsx
@@ -2,12 +2,15 @@ import { Box } from '@mui/material'
 import React from 'react'
 import { useGetPropertiesQuery } from 'src/app/services/api';
 import PropertyList from './components/PropertyList';
+import { Link } from 'react-router-dom';
 
 const Properties = () => {
     const { data: properties, isFetching: loading } = useGetPropertiesQuery();
     return (
         <Box>
-            <PropertyList loading={loading} properties={properties} />
+            <PropertyList loading={loading} properties={properties} filterComponent={<Box sx={{ mb: 1, display: 'flex', justifyContent: 'end', mx: 4, alignItems: 'center' }}>
+                <Link to='/properties/search' style={{ fontFamily: 'Plus Jakarta Sans' }}>All Filters</Link>
+            </Box>} />
         </Box>
     )
 }
