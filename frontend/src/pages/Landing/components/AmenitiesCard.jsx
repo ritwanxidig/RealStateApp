65dbaf8f9a75fe0083ca803bab2521cc3dfe49f8
ritwanxidig <ritwanrashit@gmail.com>
2024-01-08 13:54:40 +0300

    Refactor AmenitiesCard component to conditionally style based on noBg prop
    This change enables the AmenitiesCard component to conditionally apply
    background and text colors based on the presence of the noBg prop,
    ensuring flexible styling options. This update maintains visual
    consistency while allowing for greater customization. Closes #1234

--- frontend/src/pages/Landing/components/AmenitiesCard.jsx
+++ frontend/src/pages/Landing/components/AmenitiesCard.jsx
@@ -1,20 +1,19 @@
 import { Box, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 
-const AmenitiesCard = ({ Icon, title, textDecoration }) => {
+const AmenitiesCard = ({ Icon, title, textDecoration, noBg }) => {
     const { darkMode } = useSelector(state => state.theme)
     return (
         <Box
             sx={{
                 display: 'flex',
                 alignItems: 'center',
-                width: '100%',
                 gap: 1,
-                color: darkMode ? 'white' : 'primary.main',
+                color: darkMode && !noBg ? 'white' : 'primary.main',
 
             }}
         >
-            <Box sx={{ p: .5, backgroundColor: 'primary.main', color: 'white', borderRadius: '4px', }}> <Icon size={20} stroke={2.25} /></Box>
+            <Box sx={{ p: .5, backgroundColor: !noBg && 'primary.main', color: !noBg && 'white', borderRadius: '4px', }}> <Icon size={20} stroke={2.25} /></Box>
             <Typography variant="body2" fontSize='12px' sx={{ textDecoration: textDecoration }} color={darkMode ? 'primary.200' : 'primary.dark'} className='text-sm'>
                 {title}
             </Typography>
