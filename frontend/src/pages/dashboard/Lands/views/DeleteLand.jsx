f6d9aa4bf0ed7ddca427d5b827bf51dfebd40cab
ritwanxidig <ritwanrashit@gmail.com>
2024-01-27 06:28:29 +0300

    Create DeleteLand component for handling land deletion in the dashboard. The component encapsulates the deletion logic and user interface for confirming land deletion. It provides a modal with options to confirm or cancel the deletion, along with appropriate feedback messages. This enhances the user experience and ensures systematic handling of land deletions. No issue references.

---
+++ frontend/src/pages/dashboard/Lands/views/DeleteLand.jsx
@@ -0,0 +1,58 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+import { toast } from 'react-hot-toast'
+import { useDeleteLandMutation } from 'src/app/services/api'
+import StyledButton from 'src/components/shared/StyledButton'
+import CustomModal from 'src/views/utilities/CustomModal'
+
+const DeleteLand = ({ onOpen, setOnOpen, data }) => {
+    const [deleteLand, { isLoading }] = useDeleteLandMutation()
+
+    const handleDelete = async () => {
+        await toast.promise(deleteLand(data?._id).unwrap().then(() => {
+            setOnOpen(false);
+        }), {
+            loading: 'Deleting land...',
+            success: 'Land deleted successfully',
+            error: 'Error deleting land',
+        });
+    }
+
+    const actions = () => (
+        <Box sx={{ display: "flex", justifyContent: "end", gap: 1 }}>
+            <StyledButton
+                variant="contained"
+                color="error"
+                onClick={() => setOnOpen(false)}
+            >
+                Cancel
+            </StyledButton>
+            <StyledButton
+                variant="contained"
+                color="success"
+                onClick={() => handleDelete()}
+                disabled={isLoading}
+            >
+                Delete
+            </StyledButton>
+        </Box>
+    )
+
+    return (
+        <CustomModal
+            title="Delete Land"
+            onOpen={onOpen}
+            setOnOpen={setOnOpen}
+            actions={actions()}
+        >
+            <Typography variant="body2" fontFamily="Plus Jakarta Sans" fontWeight="400" fontSize="15px">
+                Are you sure you want to delete <span className='font-bold capitalize'>{data?.name}</span> land?
+            </Typography>
+            <Typography variant="body2" fontFamily={"Plus Jakarta Sans"} fontWeight="400" fontSize="13px" color="grey.400">
+                This action will remove all the cities and their locations associated with this land. and it is irreversible
+            </Typography>
+        </CustomModal>
+    )
+}
+
+export default DeleteLand
