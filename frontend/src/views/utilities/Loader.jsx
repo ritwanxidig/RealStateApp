3585517925066fe72e999f08352da9ea4edc3705
ritwanxidig <ritwanrashit@gmail.com>
2024-01-28 08:08:51 +0300

    Refactor Loader component for improved user experience
    Refactor Loader component to display loading text for enhanced user
    experience and clarity. This change adds a Typography component to show
    "Loading..." text underneath the loader icon.

--- frontend/src/views/utilities/Loader.jsx
+++ frontend/src/views/utilities/Loader.jsx
@@ -1,13 +1,14 @@
 import { useTheme } from '@emotion/react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React from 'react'
 import { CircleLoader, HashLoader } from "react-spinners";
 
 const Loader = () => {
     const theme = useTheme();
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center', backgroundColor: 'transparent', alignItems: 'center', height: '100%', color: 'primary.main' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', backgroundColor: 'transparent', alignItems: 'center', height: '100%', color: 'primary.main' }}>
             <HashLoader color={theme.palette.primary.main} />
+            <Typography variant='body' fontSize='14px' fontWeight={600} >Loading...</Typography>
         </Box>
     )
 }
