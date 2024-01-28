c00f4067a9029fb5297614be7904e8aeab590184
ritwanxidig <ritwanrashit@gmail.com>
2024-01-28 09:37:12 +0300

    Create AllProperties component for managing and displaying a list of properties. The new component integrates with API services and Redux state for property management. It incorporates features like property deletion and navigation to add new properties. This component aims to provide a comprehensive view and CRUD functionality for properties within the dashboard. #134

---
+++ frontend/src/pages/dashboard/Properties/AllProperties.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { IconButton } from '@mui/material'
+
+// for project
+import { useGetPropertiesQuery } from 'src/app/services/api'
+import { } from 'src/app/slices/alertSlice'
+import PageContainer from 'src/Layout/Main/Containers/PageContainer'
+import PageCard from 'src/Layout/Main/Containers/PageCard'
+import { IconPlus } from '@tabler/icons-react'
+import { Link } from 'react-router-dom'
+import DeleteProperty from './components/DeleteProperty'
+import { useSelector } from 'react-redux'
+import Properties_List from './list';
+
+const AllProperties = () => {
+    const [deleteOpen, setDeleteOpen] = React.useState(false);
+    // const [detailOpen, setDetailOpen] = React.useState(false);
+    const [selectedProperty, setSelectedProperty] = React.useState(null);
+    const { data: properties, isFetching } = useGetPropertiesQuery();
+    return (
+        <>
+            {deleteOpen && <DeleteProperty onOpen={deleteOpen} setOnOpen={setDeleteOpen} data={selectedProperty} />}
+            <PageContainer title='Properties' description=''>
+
+                <PageCard
+                    title="Properties List"
+                    headtitle="Properties Management"
+                    headsubtitle={"List of all properties"}
+                    subtitle="List of all your properties"
+                    action={<>
+                        <IconButton
+                            sx={{ backgroundColor: 'primary.main', color: 'white', ":hover": { backgroundColor: 'primary.dark' } }} >
+                            <Link to="/app/properties/new">
+                                <IconPlus />
+                            </Link>
+                        </IconButton>
+                    </>}
+
+                >
+                    <Properties_List properties={properties} isFetching={isFetching} setDeleteOpen={setDeleteOpen} setSelectedProperty={setSelectedProperty} />
+                </PageCard>
+            </PageContainer>
+        </>
+
+    )
+}
+
+export default AllProperties
