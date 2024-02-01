2c74642554f43439f961410421392d34cb5ae085
ritwanxidig <ritwanrashit@gmail.com>
2024-02-01 21:38:41 +0300

    Refactor PieChart component to use recharts for improved data visualization
    Replace react-chartjs-2 with recharts library to enhance data
    visualization in the PieChart component, providing a more interactive
    and customizable user experience.

--- frontend/src/pages/dashboard/AdminDash/components/charts/PieChart.jsx
+++ frontend/src/pages/dashboard/AdminDash/components/charts/PieChart.jsx
@@ -1,82 +1,105 @@
-import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
-import { Colors } from 'chart.js';
-import Chart from 'chart.js/auto';
-import React from 'react';
-import { Pie } from 'react-chartjs-2';
+import { useTheme } from '@emotion/react';
+import { Box } from '@mui/material';
+import { Legend } from 'chart.js';
+import React from 'react'
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 const PieChart = () => {
-    const [labels, setLabels] = React.useState(["January", "February", "March", "April", "May", "June", "July"]);
-    Chart.register(Colors);
-
-    const data = {
-        labels: labels,
-        datasets: [
-            {
-                label: "My First dataset",
-                data: [10, 10, 5, 45, 20, 30, 40],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.7)',
-                    'rgba(255, 159, 64, 0.7)',
-                    'rgba(255, 205, 86, 0.7)',
-                    'rgba(75, 192, 192, 0.7)',
-                    'rgba(54, 162, 235, 0.7)',
-                    'rgba(153, 102, 255, 0.7)',
-                ],
-            },
-        ],
-    };
-
-    const options = {
-        layout: {
-            padding: 10
+    const theme = useTheme();
+    // Data for the pie chart
+    const data = [
+        {
+            name: 'Jan',
+            homes: 4000,
+            lands: 2400,
+            amt: 2400,
         },
-        plugins: {
-            legend: {
-                display: false
-            },
-            tooltip: {
-                callbacks: {
-                    label: (context) => {
-                        const labelIndex = context.dataIndex;
-                        return `${labels[labelIndex]}: ${context.formattedValue}`;
-                    },
-                },
-            },
+        {
+            name: 'Feb',
+            homes: 3000,
+            lands: 1398,
+            amt: 2210,
         },
-        responsive: true,
-        maintainAspectRatio: false,
+        {
+            name: 'Mar',
+            homes: 2000,
+            lands: 9800,
+            amt: 2290,
+        },
+        {
+            name: 'Apr',
+            homes: 2780,
+            lands: 3908,
+            amt: 2000,
+        },
+        {
+            name: 'May',
+            homes: 2780,
+            lands: 3908,
+            amt: 2000,
+        },
+        {
+            name: 'Jun',
+            homes: 1890,
+            lands: 4800,
+            amt: 2181,
+        },
+        {
+            name: 'Jul',
+            homes: 2390,
+            lands: 3800,
+            amt: 2500,
+        },
+        {
+            name: 'Aug',
+            homes: 3490,
+            lands: 4300,
+            amt: 2100,
+        },
 
-    }
+        {
+            name: 'Sep',
+            homes: 1890,
+            lands: 4800,
+            amt: 2181,
+        },
+        {
+            name: 'Oct',
+            homes: 2390,
+            lands: 3800,
+            amt: 2500,
+        },
+        {
+            name: 'Nov',
+            homes: 3490,
+            lands: 4300,
+            amt: 2100,
+        },
 
-    const legendStyles = {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(7, 1fr)',
-        listStyle: 'none',
-        justifyContent: 'center',
-        gap: 2,
-        padding: 0,
-    };
-
-
+        {
+            name: 'Dec',
+            homes: 3490,
+            lands: 4300,
+            amt: 2100,
+        },
+    ];
     return (
-        <Box sx={{ width: '100%', height: '100%' }}>
-            {/* <List style={legendStyles}>
-                {labels.map((label, index) => (
-                    // when we click specific label it will remove that label from the chart
-                    <ListItem onClick={() => {
-                        const newLabels = [...labels];
-                        newLabels.splice(index, 1);
-                        setLabels(newLabels);
-                    }} key={index} style={{ margin: '0 0', borderRadius: '5px', background: data.datasets[0].backgroundColor[index] }}>
-                        <ListItemText style={{ margin: '0', padding: '0' }}>
-                            <Typography variant="body2" fontSize={12} style={{ color: '#fff' }}>{label}</Typography>
-                        </ListItemText>
-                    </ListItem>
-                ))}
-            </List> */}
-            <Pie data={data} options={options} />
-        </Box>
-    );
-};
+        // <Box sx={{ width: '100%' }}>
+        <ResponsiveContainer>
+            <AreaChart
+                data={data}
 
-export default PieChart;
+            >
+                {/* <CartesianGrid strokeDasharray="4 4" /> */}
+                <XAxis dataKey="name" />
+                
+                <Tooltip />
+                <Area type="monotone" dataKey="lands" stackId="1" stroke={theme?.palette?.secondary?.main || "#82ca9d"} fill={theme?.palette?.secondary?.main || "#82ca9d"} />
+                <Area type="monotone" dataKey="homes" stackId="1" stroke={theme?.palette?.primary?.main || "#8884d8"} fill={theme?.palette?.primary?.main || "#8884d8"} />
+            </AreaChart>
+        </ResponsiveContainer>
+        // </Box>
+    )
+}
+
+export default PieChart
