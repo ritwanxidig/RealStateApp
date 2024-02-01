9c38be9473ed743c852b1d4e0da71c4e23c904ed
ritwanxidig <ritwanrashit@gmail.com>
2024-02-01 21:38:51 +0300

    Create RadialBarChart component for dashboard
    Add a new RadialBarChart component to display data using recharts
    library. This component represents data visualization for specific
    properties and lands. The usage of recharts library ensures responsive
    and interactive chart presentation on the dashboard. The colors of the
    chart are customized based on the theme palette. This addition enhances
    the visualization capabilities of the admin dashboard.
    
    #ISSUE-123

---
+++ frontend/src/pages/dashboard/AdminDash/components/charts/RadialBarChart.jsx
@@ -0,0 +1,43 @@
+import { useTheme } from '@emotion/react';
+import React from 'react'
+import { RadialBarChart, Pie, RadialBar, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis, PieChart, Cell } from 'recharts'
+
+const RadialBarChartComponent = () => {
+    const data = [
+        {
+            name: 'properties',
+            value: 80
+        },
+        {
+            name: 'lands',
+            value: 300
+        },
+    ]
+    const theme = useTheme();
+
+    const colors = [
+        theme.palette.primary['main'],
+        theme.palette.secondary['main'],
+    ]
+
+    return (
+        <ResponsiveContainer>
+            <PieChart>
+                <Tooltip />
+                <Pie
+                    data={data}
+                    innerRadius={60}
+                    outerRadius={80}
+                    paddingAngle={2}
+                    dataKey="value"
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} stroke={colors[index % colors.length]} fill={colors[index % colors.length]} />
+                    ))}
+                </Pie>
+            </PieChart>
+        </ResponsiveContainer>
+    )
+}
+
+export default RadialBarChartComponent
