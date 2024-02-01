8f7c408c7c33ce6c68e954a0b3b081c6a15602de
ritwanxidig <ritwanrashit@gmail.com>
2024-02-01 09:25:21 +0300

    feat: Add StatsCard1 component to Admin Dashboard
    Add a new StatsCard1 component to display statistics in the Admin
    Dashboard, improving user experience and data visualization.
    
    #Issue-123

---
+++ frontend/src/pages/dashboard/AdminDash/components/StatsCard1.jsx
@@ -0,0 +1,34 @@
+import { useTheme } from '@emotion/react';
+import { Box, Typography } from '@mui/material';
+import { IconHomeStar } from '@tabler/icons-react';
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+const StatsCard1 = ({ bgColor, IconColor, textColor }) => {
+
+    const theme = useTheme();
+    const { darkMode } = useSelector((state) => state.theme);
+    return (
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 1,
+            justifyItems: 'start',
+            alignItems: 'start',
+            justifyContent: 'center',
+            backgroundColor: bgColor || "",
+            p: 2,
+            borderRadius: 2,
+            height: '12rem'
+        }}>
+            <Box sx={{ p: 1, borderRadius: '50%', backgroundColor: IconColor || "", color: 'white' }} >
+                <IconHomeStar size={18} />
+            </Box>
+            <Typography sx={{ fontWeight: 'bold', fontSize: '1.4rem', fontFamily: 'Plus Jakarta Sans' }}>$11k</Typography>
+            <Typography sx={{ fontWeight: '500', color: textColor ? textColor : darkMode ? 'grey.100' : 'grey.800', fontSize: '.9rem', fontFamily: 'Plus Jakarta Sans' }}>Total homes posted</Typography>
+        </Box>
+    )
+}
+
+
+export default StatsCard1
