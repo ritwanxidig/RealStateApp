6894714c36494df78f64896c0a6b8e636c06c60b
ritwanxidig <ritwanrashit@gmail.com>
2024-02-04 14:32:03 +0300

    Refactor tbmkFormatter to handle larger numbers
    Refactor tbmkFormatter to handle numbers greater than or equal to specified thresholds in order to accurately format larger numerical values. This change enables correct formatting for numbers in the trillions and addresses potential display inconsistencies. Issue #123

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,19 +1,19 @@
 export function tbmkFormatter(num) {
   // if it reaches 1t
-  if (num >= 999999999999) {
-    return (num / 1000000000).toFixed(1).replace(/\.0$/, "") + "t";
+  if (num >= 1000000000000) {
+    return (num / 1000000000000).toFixed(1).replace(/\.0$/, "") + "t";
   }
   // if it reaches 1b
-  if (num >= 999999999) {
-    return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "b";
+  if (num >= 1000000000) {
+    return (num / 1000000000).toFixed(1).replace(/\.0$/, "") + "b";
   }
   // if it reaches 1m
-  if (num >= 999999) {
-    return (num / 1000).toFixed(1).replace(/\.0$/, "") + "m";
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "m";
   }
   // if it reaches 1k
-  if (num >= 999) {
-    return (num / 100).toFixed(1).replace(/\.0$/, "") + "k";
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1).replace(/\.0$/, "") + "k";
   }
   return num;
 }
