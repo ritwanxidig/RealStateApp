f86246f5c7797f208fc5cbde04bcd215898c8630
ritwanxidig <ritwanrashit@gmail.com>
2024-02-04 14:33:01 +0300

    Refactor LastYearRevenue component to dynamically render pie chart data
    Update LastYearRevenue component to accept data via props and dynamically generate pie chart data, improving reusability and data flexibility. (#123)

--- frontend/src/pages/dashboard/AdminDash/components/LastYearRevenue.jsx
+++ frontend/src/pages/dashboard/AdminDash/components/LastYearRevenue.jsx
@@ -3,11 +3,18 @@ import DashboardCard from './DashboardCard'
 import { Box } from '@mui/material'
 import PieChart from './charts/PieChart'
 
-const LastYearRevenue = () => {
+const LastYearRevenue = (props) => {
+    const chartData = props?.data?.map(item => {
+        return {
+            month: item?.month,
+            homes: item?.revenue?.properties,
+            lands: item?.revenue?.lands
+        }
+    })
     return (
         <DashboardCard width='100%' title="Last Year Revenue" subtitle="Insights of last year revenue">
             <Box sx={{ overflow: 'hidden', width: '100%', display: 'flex', justifyContent: 'center', height: '13rem' }}>
-                <PieChart />
+                <PieChart data={chartData} />
             </Box>
         </DashboardCard>
     )
