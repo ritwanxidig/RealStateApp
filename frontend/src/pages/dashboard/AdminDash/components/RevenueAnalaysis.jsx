9071434682dff149eaec9d6bc8dee14019fc3693
ritwanxidig <ritwanrashit@gmail.com>
2024-02-04 14:35:10 +0300

    Refactor RevenueAnalysis component to dynamically display revenue data
    Utilize props to dynamically render revenue data in the RevenueAnalysis component instead of hardcoding values, enhancing reusability and flexibility for different datasets. This change also uses a formatter for revenue values. Closes #123.

--- frontend/src/pages/dashboard/AdminDash/components/RevenueAnalaysis.jsx
+++ frontend/src/pages/dashboard/AdminDash/components/RevenueAnalaysis.jsx
@@ -3,26 +3,44 @@ import DashboardCard from './DashboardCard'
 import { Box, Typography } from '@mui/material'
 import RadialBarChartComponent from './charts/RadialBarChart'
 import { useTheme } from '@emotion/react'
+import { tbmkFormatter } from 'src/constants'
 
-const RevenueAnalaysis = () => {
+const RevenueAnalaysis = (props) => {
     const theme = useTheme();
-    
+
+    const data = props?.data || {
+        revenueFromProperties: 5000,
+        revenueFromLands: 2000,
+    }
+
+    const chatData = [
+        {
+            name: 'Properties',
+            value: data?.revenueFromProperties
+        },
+        {
+            name: 'Lands',
+            value: data?.revenueFromLands
+        },
+    ]
+
     return (
         <DashboardCard width='100%' title="Revenue Analaysis" subtitle="Revenue Analaysis of all the updates" >
             <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '13rem' }}>
-                <RadialBarChartComponent />
+                <RadialBarChartComponent data={chatData} />
                 <Box sx={{ display: 'flex', width: '100%', justifyContent: 'center', gap: 5 }}>
                     <Box sx={{ display: 'flex', gap: .5, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', }}>
                         <Box sx={{ display: 'flex', gap: .4, justifyContent: 'center', alignItems: 'center', }}>
                             <div className={`w-2 h-2   rounded-full`} style={{ backgroundColor: theme.palette.primary.main }} /> <Typography sx={{ fontWeight: '500', fontSize: '1rem', fontFamily: 'Plus Jakarta Sans' }}>Properties</Typography>
                         </Box>
-                        <Typography sx={{ fontWeight: 'bold', fontSize: '.8rem', fontFamily: 'Plus Jakarta Sans' }}>$5000</Typography>
+                        <Typography sx={{ fontWeight: 'bold', fontSize: '1rem', fontFamily: 'Plus Jakarta Sans' }}>${tbmkFormatter(data?.revenueFromProperties)}</Typography>
                     </Box>
                     <Box sx={{ display: 'flex', gap: .5, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', }}>
                         <Box sx={{ display: 'flex', gap: .4, justifyContent: 'center', alignItems: 'center', }}>
-                            <div className={`w-2 h-2   rounded-full`} style={{ backgroundColor: theme.palette.primary.main }} /> <Typography sx={{ fontWeight: '500', fontSize: '1rem', fontFamily: 'Plus Jakarta Sans' }}>Lands</Typography>
+                            <div className={`w-2 h-2   rounded-full`} style={{ backgroundColor: theme.palette.primary.main }} />
+                            <Typography sx={{ fontWeight: '500', fontSize: '1rem', fontFamily: 'Plus Jakarta Sans' }}>Lands</Typography>
                         </Box>
-                        <Typography sx={{ fontWeight: 'bold', fontSize: '.8rem', fontFamily: 'Plus Jakarta Sans' }}> $400</Typography>
+                        <Typography sx={{ fontWeight: 'bold', fontSize: '1rem', fontFamily: 'Plus Jakarta Sans' }}> ${tbmkFormatter(data?.revenueFromLands)}</Typography>
                     </Box>
                 </Box>
             </Box>
