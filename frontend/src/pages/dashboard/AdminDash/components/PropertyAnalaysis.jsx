8873e206fe1dc00f6e4b37b5d102e384134bcd69
ritwanxidig <ritwanrashit@gmail.com>
2024-02-04 14:35:01 +0300

    Refactor PropertyAnalysis to Dynamically Render Data
    Modify PropertyAnalysis component to dynamically render property data, improving reusability and scalability. Utilize tbmkFormatter function for value display. No associated issues.

--- frontend/src/pages/dashboard/AdminDash/components/PropertyAnalaysis.jsx
+++ frontend/src/pages/dashboard/AdminDash/components/PropertyAnalaysis.jsx
@@ -3,26 +3,43 @@ import DashboardCard from './DashboardCard'
 import { Box, Typography } from '@mui/material'
 import RadialBarChartComponent from './charts/RadialBarChart'
 import { useTheme } from '@emotion/react'
+import { tbmkFormatter } from 'src/constants'
 
-const PropertyAnalaysis = () => {
+const PropertyAnalaysis = (props) => {
     const theme = useTheme();
+
+    const formattedData = props?.data?.map((item) => {
+        return {
+            name: item?._id,
+            value: item?.count
+        }
+    })
+
+    const data = formattedData || [
+        {
+            name: 'rent',
+            value: 52
+        },
+        {
+            name: 'sale',
+            value: 28
+        },
+    ]
+
     return (
         <DashboardCard width='100%' title="Properties Analaysis" subtitle="Properties Analaysis based on types" >
             <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '13rem' }}>
-                <RadialBarChartComponent />
+                <RadialBarChartComponent data={data} />
                 <Box sx={{ display: 'flex', mt: 2, width: '100%', justifyContent: 'center', gap: 5 }}>
-                    <Box sx={{ display: 'flex', gap: .5, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', }}>
-                        <Box sx={{ display: 'flex', gap: .4, justifyContent: 'center', alignItems: 'center', }}>
-                            <div className={`w-2 h-2   rounded-full`} style={{ backgroundColor: theme.palette.primary.main }} /> <Typography sx={{ fontWeight: '500', fontSize: '1rem', fontFamily: 'Plus Jakarta Sans' }}>Rent</Typography>
+                    {data?.map((item) => (
+                        <Box key={item.name} sx={{ display: 'flex', gap: .5, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', }}>
+                            <Box sx={{ display: 'flex', gap: .4, justifyContent: 'center', alignItems: 'center', }}>
+                                <div className={`w-2 h-2   rounded-full`} style={{ backgroundColor: theme.palette.primary.main }} />
+                                <Typography sx={{ fontWeight: '500', fontSize: '1rem', fontFamily: 'Plus Jakarta Sans', textTransform: 'capitalize' }}>{item?.name}</Typography>
+                            </Box>
+                            <Typography sx={{ fontWeight: 'bold', fontSize: '.8rem', fontFamily: 'Plus Jakarta Sans' }}>{tbmkFormatter(item?.value)}</Typography>
                         </Box>
-                        <Typography sx={{ fontWeight: 'bold', fontSize: '.8rem', fontFamily: 'Plus Jakarta Sans' }}>52</Typography>
-                    </Box>
-                    <Box sx={{ display: 'flex', gap: .5, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', }}>
-                        <Box sx={{ display: 'flex', gap: .4, justifyContent: 'center', alignItems: 'center', }}>
-                            <div className={`w-2 h-2   rounded-full`} style={{ backgroundColor: theme.palette.primary.main }} /> <Typography sx={{ fontWeight: '500', fontSize: '1rem', fontFamily: 'Plus Jakarta Sans' }}>Sale</Typography>
-                        </Box>
-                        <Typography sx={{ fontWeight: 'bold', fontSize: '.8rem', fontFamily: 'Plus Jakarta Sans' }}> 34</Typography>
-                    </Box>
+                    ))}
                 </Box>
             </Box>
         </DashboardCard>
