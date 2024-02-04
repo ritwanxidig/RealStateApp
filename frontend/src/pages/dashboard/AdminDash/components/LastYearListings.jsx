e4db001dda8f64aad1c664f10b7e35400f60c4f2
ritwanxidig <ritwanrashit@gmail.com>
2024-02-04 14:32:44 +0300

    Refactor LastYearListings to Dynamically Render Chart Data
    Extract chart data mapping logic into props, improving reusability and flexibility. Enhance component scalability for diverse data sources. Addresses #123.

--- frontend/src/pages/dashboard/AdminDash/components/LastYearListings.jsx
+++ frontend/src/pages/dashboard/AdminDash/components/LastYearListings.jsx
@@ -3,11 +3,18 @@ import DashboardCard from './DashboardCard'
 import { Box } from '@mui/material'
 import BarChartComponent from './charts/BarChart'
 
-const LastYearListings = () => {
+const LastYearListings = (props) => {
+    const chartData = props?.data?.map(item => {
+        return {
+            month: item?.month,
+            homes: item?.published?.properties,
+            lands: item?.published?.lands
+        }
+    })
     return (
         <DashboardCard width='100%' title="Last Year Listings" subtitle="Insights of last year listings">
             <Box sx={{ overflow: 'hidden', width: '100%', display: 'flex', justifyContent: 'center', height: '13rem' }}>
-                <BarChartComponent />
+                <BarChartComponent data={chartData} />
             </Box>
         </DashboardCard>
     )
