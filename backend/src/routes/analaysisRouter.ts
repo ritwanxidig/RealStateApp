ee8c80e2cfb302fdec3992fc3367852411fa5398
ritwanxidig <ritwanrashit@gmail.com>
2024-02-04 14:40:32 +0300

    Refactor route to use AdminAnalaysisController
    Changed the route to utilize AdminAnalaysisController for more specifically targeted functionality. This change improves code organization and ensures that admin-level analysis operations are handled separately. Thus, it enhances maintainability and scalability. No associated issue.

--- backend/src/routes/analaysisRouter.ts
+++ backend/src/routes/analaysisRouter.ts
@@ -1,6 +1,6 @@
-import AnalaysisController from "../controllers/AnalaysisController";
+import {AdminAnalaysisController} from "../controllers/AnalaysisController";
 import { Router } from "express";
 
 export default (router: Router) => {
-  router.get("/analaysis", AnalaysisController.getOverviewAnalaysis);
+  router.get("/analaysis", AdminAnalaysisController.getOverviewAnalaysis);
 };
