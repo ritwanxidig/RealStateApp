34540503d44f047542a38b805b010fbce72d665b
ritwanxidig <ritwanrashit@gmail.com>
2024-02-04 14:34:11 +0300

    Refactor MostActiveCities component to dynamically render data
    Utilize props to dynamically render city data, ensuring flexibility and reusability of the component. Improve maintainability. Fixes #123.

--- frontend/src/pages/dashboard/AdminDash/components/MostActiveCities.jsx
+++ frontend/src/pages/dashboard/AdminDash/components/MostActiveCities.jsx
@@ -2,46 +2,42 @@ import React from 'react'
 import DashboardCard from './DashboardCard'
 import { Box, Chip } from '@mui/material'
 import { useTheme } from '@emotion/react';
+import { tbmkFormatter } from 'src/constants';
 
-const MostActiveCities = () => {
+const MostActiveCities = (props) => {
     const theme = useTheme();
+
+    const data = props.data || [
+        {
+            name: 'Hargeisa',
+            properties: '2',
+            lands: '1',
+            revenue: '500'
+        }
+    ]
     return (
         <DashboardCard width='100%' title="Most Active Cities" subtitle="Top 3 most active cities">
             <Box sx={{ width: '100%' }}>
                 <table className='min-w-full text-center text-sm shadow-lg rounded-xl'>
                     <thead className=''>
                         <tr>
-                            <th className='text-start pl-4 py-2'>City</th>
+                            <th className='text-start pl-4 py-2'>Name</th>
                             <th className='text-start pl-4 py-2'>Properties</th>
                             <th className='text-start pl-4 py-2'>Lands</th>
                             <th className='text-start pl-4 py-2'>Revenue</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className={` rounded-xl hover:shadow-lg hover:shadow-slate-200 transition-all duration-500 cursor-default`}>
-                            <td className='py-4 text-start pl-4 font-medium'>Hargeisa</td>
-                            <td className='py-4 text-start pl-4 font-medium'>2</td>
-                            <td className='py-4 text-start pl-4 font-medium'>1</td>
-                            <td className='py-4 text-start pl-4 font-medium'>
-                                <Chip color='success' size='small' label={`$ ${200.00}`} />
-                            </td>
-                        </tr>
-                        <tr className={` rounded-xl hover:shadow-lg hover:shadow-slate-200 transition-all duration-500 cursor-default`}>
-                            <td className='py-4 text-start pl-4 font-medium'>Hargeisa</td>
-                            <td className='py-4 text-start pl-4 font-medium'>2</td>
-                            <td className='py-4 text-start pl-4 font-medium'>1</td>
-                            <td className='py-4 text-start pl-4 font-medium'>
-                                <Chip color='success' size='small' label={`$ ${200.00}`} />
-                            </td>
-                        </tr>
-                        <tr className={` rounded-xl hover:shadow-lg hover:shadow-slate-200 transition-all duration-500 cursor-default`}>
-                            <td className='py-4 text-start pl-4 font-medium'>Hargeisa</td>
-                            <td className='py-4 text-start pl-4 font-medium'>2</td>
-                            <td className='py-4 text-start pl-4 font-medium'>1</td>
-                            <td className='py-4 text-start pl-4 font-medium'>
-                                <Chip color='success' size='small' label={`$ ${200.00}`} />
-                            </td>
-                        </tr>
+                        {data?.map((item, index) => (
+                            <tr key={item.name + item.properties + index + Date.now()} className={` rounded-xl hover:shadow-lg hover:shadow-slate-200 transition-all duration-500 cursor-default`}>
+                                <td className='py-4 text-start pl-4 font-medium'>{item.name}</td>
+                                <td className='py-4 text-start pl-4 font-medium'>{item.properties}</td>
+                                <td className='py-4 text-start pl-4 font-medium'>{item.lands}</td>
+                                <td className='py-4 text-start pl-4 font-medium'>
+                                    <Chip color='success' size='small' label={`$ ${tbmkFormatter(item.revenue)}`} />
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </Box>
