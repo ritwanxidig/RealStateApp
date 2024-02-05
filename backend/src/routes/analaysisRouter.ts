3b43800322e3770e79d3a5ba45f835f766f31589
ritwanxidig <ritwanrashit@gmail.com>
2024-02-05 15:35:19 +0300

    Refactor analysis route to add authorization for admin role
    Add authorization middleware to restrict access to admin-only analysis overview. It ensures that only users with the admin role can access the specified route. This enhances security and access control for sensitive data.

--- backend/src/routes/analaysisRouter.ts
+++ backend/src/routes/analaysisRouter.ts
@@ -1,14 +1,16 @@
-import { IsAuthenticated } from "../middleware";
+import { IsAuthenticated, IsAuthorized } from "../middleware";
 import {
   AdminAnalaysisController,
   UserAnalaysisController,
 } from "../controllers/AnalaysisController";
 import { Router } from "express";
+import { Roles } from "../constants";
 
 export default (router: Router) => {
   router.get(
     "/analaysis",
     IsAuthenticated,
+    IsAuthorized([Roles.Admin]),
     AdminAnalaysisController.getOverviewAnalaysis
   );
   router.get(
