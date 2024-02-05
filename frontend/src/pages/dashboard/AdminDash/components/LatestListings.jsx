72c81ca622c662a393d7c99ee56c941aefa9dbf2
ritwanxidig <ritwanrashit@gmail.com>
2024-02-05 15:35:57 +0300

    Refactor grid layout to improve responsiveness and accommodate varying screen sizes
    Adjust grid column settings to optimize display and scrolling in smaller screens. This change ensures a better user experience across different devices. No related issue is addressed in this commit.

--- frontend/src/pages/dashboard/AdminDash/components/LatestListings.jsx
+++ frontend/src/pages/dashboard/AdminDash/components/LatestListings.jsx
@@ -44,10 +44,11 @@ const LatestListings = (props) => {
         <DashboardCard width='100%' title="Latest Listings" subtitle="Latest Listings added" >
             <Box sx={{
                 display: 'grid',
-                gridTemplateColumns: { lg: 'repeat(4, 1fr)', md: 'repeat(3, 1fr)', sm: 'repeat(3, 1fr)', xs: 'repeat(1, 1fr)' },
+                gridTemplateColumns: { lg: 'repeat(4, 1fr)', md: 'repeat(4, 1fr)', sm: 'repeat(3, 1fr)', xs: 'repeat(2, 1fr)' },
                 gap: 2, width: '100%',
                 justifyContent: 'center',
                 alignItems: 'center',
+                overflow: 'auto',
             }}>
                 {
                     data?.map((item, index) => (
